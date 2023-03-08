46747cc9f799be68ab9217b5c3b7a8152dd65565
Sebastiano Mura <sebamura64@gmail.com>
2023-03-08 16:58:25 +0100

    esercizio3

---
+++ esercizio3/es3.js
@@ -0,0 +1,37 @@
+
+ ((2 + 4) * (5 + 2) - 2) / 5
+
+ const sum = (a, b) => a + b;
+
+ const subtract = (a, b) => a - b;
+ 
+ const multiply = (a, b) => a * b; 
+ 
+ const divide = (a, b) => a / b; 
+
+const log = (value) => console.log(value); 
+
+let tot = divide(subtract(multiply(sum(2, 4), sum(5, 2)), 2), 5);
+
+log(tot);
+
+
+/* const sum = function (a, b) {
+    return a + b;
+  }
+  
+  const subtract = function (a, b) {
+    return a - b;
+  }
+  
+  const multiply = function (a, b) {
+    return a * b;
+  }
+  
+  const divide = function (a, b) {
+    return a / b;
+  }
+  
+  const log = function (value) {
+    console.log(value);
+  } */
