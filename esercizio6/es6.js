01d56920e369f7c887cbf630d6059b97b19fd8b7
Sebastiano Mura <sebamura64@gmail.com>
2023-03-09 10:10:15 +0100

    esercizio6

---
+++ esercizio6/es6.js
@@ -0,0 +1,12 @@
+function closureprintNamehelloName () {
+    const name = "Hello John";
+    return name;
+}
+
+function innerhelloName () {
+    const name1 = "Hello John";
+    return name1;
+}
+
+console.log(closureprintNamehelloName());
+console.log(innerhelloName());
